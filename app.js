afabb44129654e293d9aa6332e9d494f66f36ba9
khel <obidos.michael.cautiber@gmail.com>
2025-01-28 15:25:37 +0800

    routing & html pages

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const app = express()
 
 //listen for request
 app.listen(3000)
+
 app.get('/', (req, res) =>{
-  res.send('<p>home page</p>');
+  // res.send('<p>home page</p>');
+  res.sendFile('./views/index.html', {root: __dirname})
+}) 
+
+app.get('/about', (req, res) =>{
+  // res.send('<p>about page</p>');
+  res.sendFile('./views/about.html', {root: __dirname})
 }) 
